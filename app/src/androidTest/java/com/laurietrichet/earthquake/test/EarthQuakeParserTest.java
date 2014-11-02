90e2fd6a8f6b8f0cabd4350d34ed74871a6bf19c
Laurie Trichet-Fougere <laurie.trichet@gmail.com>
2014-11-02 14:08:18 +1100

    fix test test that there is no redundant occurrence in the parsed array test that an object in the json array is in the parsed array test that a parsed object is corresponding to its json object

--- app/src/androidTest/java/com/laurietrichet/earthquake/test/EarthQuakeParserTest.java
+++ app/src/androidTest/java/com/laurietrichet/earthquake/test/EarthQuakeParserTest.java
@@ -12,6 +12,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.InputStream;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -27,7 +28,7 @@ import static com.laurietrichet.earthquake.net.parsers.EarthQuakeParser.TIMEDATE
 
 public class EarthQuakeParserTest extends InstrumentationTestCase {
 
-    String json = null;
+    private String json;
 
     public void setUp() throws Exception {
         super.setUp();
@@ -44,20 +45,26 @@ public class EarthQuakeParserTest extends InstrumentationTestCase {
         Assert.assertTrue(json != null);
         JSONObject jsonResponse = new JSONObject (json);
         List <EarthQuake> arrayEarthQuakes = EarthQuakeParser.parse(json);
-
+        Assert.assertTrue(arrayEarthQuakes.size() == 18);
         int index = 0;
         JSONArray jsonArray = jsonResponse.getJSONArray(EARTHQUAKES);
         JSONObject jsonItem = jsonArray.getJSONObject(index);
-        EarthQuake item = arrayEarthQuakes.get(index);
 //{"src":"us","eqid":"c000is61","timedate":"2013-07-29 22:22:48","lat":"7.6413","lon":"93.6871",
 // "magnitude":"4.6","depth":"40.90","region":"Nicobar Islands, India region"}
+        EarthQuake item = new EarthQuake.Builder ().eqid(jsonItem.getString(EQID)).build();
+
+        int pos = Collections.binarySearch(arrayEarthQuakes,item,EarthQuake.getEqidComparator());
+        Assert.assertTrue(pos >= 0);
+
+        item = arrayEarthQuakes.get(pos);
+
         Assert.assertTrue(item.getSrc().equals(jsonItem.getString(SRC)));
         Assert.assertTrue(item.getEqid().equals(jsonItem.getString(EQID)));
         Date dateFromJson = EarthQuake.getDateFormat().parse(jsonItem.optString(TIMEDATE, null));
         Assert.assertTrue(item.getTimedate().equals(dateFromJson));
         Assert.assertTrue(item.getRegion().equals(jsonItem.getString(REGION)));
         Assert.assertTrue(item.getLat().equals(Float.parseFloat(jsonItem.optString(LAT, null))));
-        Assert.assertTrue(item.getLat().equals(Float.parseFloat(jsonItem.optString(LON, null))));
+        Assert.assertTrue(item.getLon().equals(Float.parseFloat(jsonItem.optString(LON, null))));
         Assert.assertTrue(item.getDepth() == jsonItem.getDouble(DEPTH));
         Assert.assertTrue(item.getMagnitude() == jsonItem.getDouble(MAGNITUDE));
     }
